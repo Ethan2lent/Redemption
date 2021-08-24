481f0a292d2189f799ee28f456150bafec668e42
EthanT <ethanmtolentino@gmail.com>
2021-08-25 05:57:21 +1000

    Mega Gold Changes

---
+++ Assets/Scripts/TreasureDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureDoor : MonoBehaviour
+{
+    public int treasureCounter;
+    void Start()
+    {
+        treasureCounter = 0;
+    }
+
+    // Update is called once per frames
+    void Update()
+    {
+        if (treasureCounter == 4)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void treasureCount()
+    {
+        treasureCounter += 1;
+    }
+}
